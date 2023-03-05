dca96a330103a7f5c5d17b58c21c8022ecd8b335
Odds <panerjeffryjames@gmail.com>
2023-03-05 18:02:21 +0800

    replace the node_modules with new one and add package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}",
+    "./files/**/*.{html,js}",
+    "./index.html",
+
+  ],
   theme: {
     extend: {},
   },
