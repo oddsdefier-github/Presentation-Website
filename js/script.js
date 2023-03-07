62a1e4f45f152ac353303a46d99bc2cdf042e02e
Odds <panerjeffryjames@gmail.com>
2023-03-07 22:13:41 +0800

    reset

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 
-
+function closeBtn() {
+    var close = document.getElementById("f-screen")
+    if (close.style.display === "none") {
+        close.style.display = "block";
+    } else {
+        close.style.display = "none";
+    }
+    }
+    
 var hide = document.getElementById("alert");
 hide.style.display = "none";
+
+
 
 const alertTimeout = setTimeout(tipAlert, 2000);    
 
 function tipAlert() {
     var show = document.getElementById("alert")
     show.style.display = "flex";
-    window.blur()
 
 
 }
-
-
 
 
 function hideAlert() {
@@ -30,3 +37,24 @@ function stopTimeout() {
 
 }
     
+var elem = document.documentElement;
+    function openFullscreen() {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    }
+
+
+    function closeFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
+    }
